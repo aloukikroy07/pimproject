553749860471f0dc1b81ac530d85e730ea87d26c
User <User@DESKTOP-ETNQBOE>
2022-02-03 13:32:42 +0600

    Uploaded new project to git.

---
+++ PIMProject/src/main/java/com/pim/PIMProject/PimProjectApplication.java
@@ -0,0 +1,13 @@
+package com.pim.PIMProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PimProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PimProjectApplication.class, args);
+	}
+
+}
---
+++ PIMProject/src/test/java/com/pim/PIMProject/PimProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pim.PIMProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PimProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
