3a0ae4c00deeb58f9823b3e6ee6190ea9e6f712d
User <User@DESKTOP-NJN53B5>
2022-02-09 12:52:16 +0600

    customer accouts insertion completed

--- PIMProject/src/main/java/com/pim/repository/UserRegistrationRepository.java
+++ PIMProject/src/main/java/com/pim/repository/UserRegistrationRepository.java
@@ -12,6 +12,7 @@ import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 import com.pim.PIMProject.Model.CustomerProfiles;
+import com.pim.PIMProject.Model.FinancialInstitutionInfo;
 import com.pim.PIMProject.Model.Info;
 import com.pim.PIMProject.Model.RegisterUser;
 
@@ -37,22 +38,33 @@ public class UserRegistrationRepository {
 	    String formattedDate = myDateObj.format(myFormatObj);
 	    
 		Info info = ru.getEntity().getInfo();
+		FinancialInstitutionInfo fi = ru.getEntity().getFinancialInstitutionInfo();
 		
 		String sqll = "select * from t_customer_profiles where nid = '"+ru.getEntity().getInfo().getnID()+"'";
 		List <CustomerProfiles> userData = jdbcTemplate.query(sqll, BeanPropertyRowMapper.newInstance(CustomerProfiles.class));
-		
+		 /*ACTIVATED_BY,*/
 		if (userData.isEmpty()) {
 			String sql = "insert into t_customer_profiles (APPLICATION_ID, ENTITY_TYPE, MOBILE_NO, EMAIL, BRANCH_ID, CUSTOMER_ID,"
 					+ " CUSTOMER_NAME, CUSTOMER_ADDRESS,"+ " GENDER, POST_CODE, NID, TIN_NO, PASSWORD, IDTP_PIN, APP_PASSWORD, "
-					+ "FIN_NAME,"+" PRIMARY_ACCOUNT_NO, BIRTH_DATE, REGISTRATION_DATE, ACTIVATION_DATE, REMARKS, "
-					+ "TRANS_COUNT, TODAY_TRANS_AMT, TICKET_SIZE, DAILY_LIMIT)"
+					+ "FIN_NAME,"+" PRIMARY_ACCOUNT_NO, BIRTH_DATE, REGISTRATION_DATE, ACTIVATION_DATE,  ACTIVATED_BY,"
+					+ "REMARKS, TRANS_COUNT, TODAY_TRANS_AMT, TICKET_SIZE, DAILY_LIMIT)"
 					
 					+ " values (0, '"+info.getEntityType()+"', '"+info.getMobileNumber()+"', '"+info.getEmail()+"', 2, 0,"
 					+ " 'Customer Name', 'Customer Address',"+" 'Gender',"+info.getPostalCode()+", '"+info.getnID()+"', '"+info.gettIN()+"', '"+info.getPassword()
 					+"', '"+info.getiDTPKey()+"', 'App Password', 'Fin Name', 'Primary Acc No.', to_date('"+info.getDateOfBirth()+"', 'dd-mm-yyyy'),"
-							+ "to_date('"+formattedDate+"', 'dd-mm-yyyy'), to_date('"+formattedDate+"', 'dd-mm-yyyy'), 'Remarks', 0, 0, 0, 0)";
+							+ "to_date('"+formattedDate+"', 'dd-mm-yyyy'), to_date('"+formattedDate+"', 'dd-mm-yyyy'), 1, 'Remarks', 0, 0, 0, 0)";
 			
 			int insertion = jdbcTemplate.update(sql);
+			
+			if (insertion == 1) {
+				userData = jdbcTemplate.query(sqll, BeanPropertyRowMapper.newInstance(CustomerProfiles.class));
+				String sql2 = "insert into t_customer_accounts (PROFILE_ID, ACCOUNT_NO, ACC_TITLE, BRANCH_ID, REG_TYPE, DEBIT_ALLOWED, STATUS)"
+						+ "values ("+userData.get(0).getId()+", "+fi.getAccountNumber()+", 'Acc_title', 1,'B', '1',1)";
+				
+				 insertion = jdbcTemplate.update(sql2);
+			}
+			
+			
 			
 			return insertion;
 		}
