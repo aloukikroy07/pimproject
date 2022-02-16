8e6b400f347a720e90fe1b88333d04bd6d6b2a6e
User <User@DESKTOP-ETNQBOE>
2022-02-16 15:59:48 +0600

    eeeee

---
+++ PIMProject/src/main/java/com/pim/PIMProject/Response/Model/GetFIUserInfoResponse.java
@@ -0,0 +1,89 @@
+package com.pim.PIMProject.Response.Model;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
+
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
+public class GetFIUserInfoResponse {
+	@JacksonXmlProperty(localName = "Code")
+	private String code;
+	@JacksonXmlProperty(localName = "Message")
+	private String message;
+	@JacksonXmlProperty(localName = "NID")
+	private String nid;
+	@JacksonXmlProperty(localName = "BIN")
+	private String bin;
+	@JacksonXmlProperty(localName = "DateOfBirth")
+	private String dateOfBirth;
+	@JacksonXmlProperty(localName = "DateOfIncorporation")
+	private String dateOfIncorporation;
+	@JacksonXmlProperty(localName = "MobileNo")
+	private String mobileNo;
+	@JacksonXmlProperty(localName = "AccountInfo")
+	private AccountInfo accountInfo;
+	public GetFIUserInfoResponse() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public GetFIUserInfoResponse(String code, String message, String nid, String bin, String dateOfBirth,
+			String dateOfIncorporation, String mobileNo, AccountInfo accountInfo) {
+		super();
+		this.code = code;
+		this.message = message;
+		this.nid = nid;
+		this.bin = bin;
+		this.dateOfBirth = dateOfBirth;
+		this.dateOfIncorporation = dateOfIncorporation;
+		this.mobileNo = mobileNo;
+		this.accountInfo = accountInfo;
+	}
+	public String getCode() {
+		return code;
+	}
+	public void setCode(String code) {
+		this.code = code;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public String getNid() {
+		return nid;
+	}
+	public void setNid(String nid) {
+		this.nid = nid;
+	}
+	public String getBin() {
+		return bin;
+	}
+	public void setBin(String bin) {
+		this.bin = bin;
+	}
+	public String getDateOfBirth() {
+		return dateOfBirth;
+	}
+	public void setDateOfBirth(String dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+	public String getDateOfIncorporation() {
+		return dateOfIncorporation;
+	}
+	public void setDateOfIncorporation(String dateOfIncorporation) {
+		this.dateOfIncorporation = dateOfIncorporation;
+	}
+	public String getMobileNo() {
+		return mobileNo;
+	}
+	public void setMobileNo(String mobileNo) {
+		this.mobileNo = mobileNo;
+	}
+	public AccountInfo getAccountInfo() {
+		return accountInfo;
+	}
+	public void setAccountInfo(AccountInfo accountInfo) {
+		this.accountInfo = accountInfo;
+	}
+	
+}
---
+++ PIMProject/src/main/java/com/pim/PIMProject/Response/Model/GetRTPListReceivedResponse.java
@@ -0,0 +1,43 @@
+package com.pim.PIMProject.Response.Model;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
+
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
+public class GetRTPListReceivedResponse {
+	@JacksonXmlProperty(localName = "Code")
+	private String code;
+	@JacksonXmlProperty(localName = "Message")
+	private String message;
+	@JacksonXmlProperty(localName = "RTP")
+	private RTP rtp;
+	public GetRTPListReceivedResponse() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public GetRTPListReceivedResponse(String code, String message, RTP rtp) {
+		super();
+		this.code = code;
+		this.message = message;
+		this.rtp = rtp;
+	}
+	public String getCode() {
+		return code;
+	}
+	public void setCode(String code) {
+		this.code = code;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public RTP getRtp() {
+		return rtp;
+	}
+	public void setRtp(RTP rtp) {
+		this.rtp = rtp;
+	}
+	
+}
